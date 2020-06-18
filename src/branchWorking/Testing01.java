fb45a0d3a8728592ad26a2764342a13344e3de49
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-18 12:02:03 -0400

    classpath

--- src/branchWorking/Testing01.java
+++ src/branchWorking/Testing01.java
@@ -1,9 +1,26 @@
 package branchWorking;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Testing01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Test
+	public void openFirefox() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\BatchMarch2020\\jars\\chromedriver_win32\\chromedriver.exe");		
+		
+		 WebDriver driver=new ChromeDriver();
+			
+			driver.get("https://www.facebook.com/");
+			
+			System.out.println(driver.getCurrentUrl());
+			System.out.println(driver.getTitle());
+			
+		driver.findElement(By.cssSelector("#month")).sendKeys("Dec");
+		driver.findElement(By.cssSelector("#day")).sendKeys("12");
+		driver.findElement(By.cssSelector("#year")).sendKeys("1971");
 
 	}
 
