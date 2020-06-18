15d9fa7bb8857f206b4d7a75da9d5ef4518fc50b
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-18 12:26:01 -0400

    classpath done

--- src/branchWorking/Testing01.java
+++ src/branchWorking/Testing01.java
@@ -2,25 +2,45 @@ package branchWorking;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class Testing01 {
 
 	@Test
 	public void openFirefox() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\BatchMarch2020\\jars\\chromedriver_win32\\chromedriver.exe");		
 		
-		 WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\NAN2020\\drivers\\geckodriver.exe");
+		
+		 WebDriver driver=new FirefoxDriver();
 			
 			driver.get("https://www.facebook.com/");
+			driver.manage().window().maximize();
 			
-			System.out.println(driver.getCurrentUrl());
-			System.out.println(driver.getTitle());
+			driver.findElement(By.xpath("//*[@id=\"u_0_m\"]")).sendKeys("Mahfuj");//input first name
+			driver.findElement(By.xpath("//*[@id=\"u_0_o\"]")).sendKeys("Tuhin");//input last name
+			driver.findElement(By.xpath("//*[@id=\"u_0_r\"]")).sendKeys("mahfujbp@gmail.com");//input email address
+			driver.findElement(By.xpath("//*[@id=\"u_0_u\"]")).sendKeys("mahfujbp@gmail.com");//re-enter email address
+			driver.findElement(By.xpath("//*[@id=\"u_0_w\"]")).sendKeys("Mypasswordis917");//input password
 			
-		driver.findElement(By.cssSelector("#month")).sendKeys("Dec");
-		driver.findElement(By.cssSelector("#day")).sendKeys("12");
-		driver.findElement(By.cssSelector("#year")).sendKeys("1971");
+			//select date of birth
+			
+			WebElement web=driver.findElement(By.xpath("//*[@id=\"month\"]"));
+			WebElement webd=driver.findElement(By.xpath("//*[@id=\"day\"]"));
+			WebElement weby=driver.findElement(By.xpath("//*[@id=\"year\"]"));
+			
+			Select sel=new Select(web);
+			Select seld=new Select(webd);
+		    Select sely=new Select(weby);
+
+			sel.selectByVisibleText("Dec");
+			seld.selectByIndex(16);
+			sely.selectByValue("1971");
+			
+			driver.findElement(By.xpath("//*[@id=\"u_0_7\"]")).click();//click male button
+			driver.findElement(By.xpath("//*[@id=\"u_0_13\"]")).click();//click SIGNUP button
 
 	}
 
